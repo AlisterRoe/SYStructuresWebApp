bddd098f38398192226b108ebc932cef968cd0d3
Alister <roalister@gmail.com>
2021-07-27 17:16:19 +1000

    Functional sign up page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Signup from './Signup'
 import { Container } from 'react-bootstrap'
+import { AuthProvider } from '../context/AuthContext'
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className="w100" style={{ maxWidth: "400px" }}>
-        <Signup />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   )
 }
 
