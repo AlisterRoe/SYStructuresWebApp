c68d146e15c09e1c9a701302e3d64c3e6e65f226
Alister <roalister@gmail.com>
2021-07-27 17:46:11 +1000

    Fetching from firebase and low level routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,25 @@ import React from 'react'
 import Signup from './Signup'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../context/AuthContext'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login'
 
 function App() {
   return (
-    <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
         <div className="w100" style={{ maxWidth: "400px" }}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard}/>
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   )
 }
 
